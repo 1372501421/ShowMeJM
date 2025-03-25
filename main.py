09f528d207ec865b1b8f59765d73c258c66e190b
1372501421 <49863054+1372501421@users.noreply.github.com>
2025-03-25 12:25:17 +0800

    main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import aiohttp
 class MyPlugin(BasePlugin):
     # napcat的域名和端口号
     # 使用时需在napcat内配置http服务器 host和port对应好
-    http_host = "172.17.0.10"
+    http_host = "172.17.0.12"
     http_port = 3000
     # 打包成pdf时每批处理的图片数量 每批越小占用越小
     batch_size = 10
